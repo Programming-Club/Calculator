87f33a2f587df9867b75064678bb03b2b8f6a045
Tommsy64 <thomas.pakh@gmail.com>
2013-12-18 09:35:49 -0500

    Added double add subtract, multiply, divide to calculator class

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -16,4 +16,20 @@ public class Calculator {
 	public static int divide(int first, int second) {
 		return first / second;
 	}
+	
+	public static double add(double first, double second) {
+		return first + second;
+	}
+
+	public static double subtract(double first, double second) {
+		return first - second;
+	}
+
+	public static double multiply(double first, double second) {
+		return first * second;
+	}
+
+	public static double divide(double first, double second) {
+		return first / second;
+	}
 }
